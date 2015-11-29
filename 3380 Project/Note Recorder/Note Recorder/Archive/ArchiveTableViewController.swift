9a2caf573d2d2b71fa2e1c69806a2b6571e4c8df
Michael Vedros <mlawnc@gmail.com>
2015-11-28 23:20:35 -0600

    Version of the Archiver that includes a recording for testing purposes.

--- 3380 Project/Note Recorder/Note Recorder/Archive/ArchiveTableViewController.swift
+++ 3380 Project/Note Recorder/Note Recorder/Archive/ArchiveTableViewController.swift
@@ -53,6 +53,8 @@ class ArchiveTableViewController: UITableViewController, UITextFieldDelegate{
             
         }
         
+        let testRecording = Recording(name: "Test")
+        rootFolder.recordings.append(testRecording)
         currentFolder = rootFolder
         
 
